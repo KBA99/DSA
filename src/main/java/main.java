7f0ad072d611982b963f29fb440e64b4a3dfac12
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-12-27 15:55:00 +0000

    feat: add main class

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        var list = new LinkedList();
+
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+
+
+        System.out.println(list.getSize());
+    }
+}
